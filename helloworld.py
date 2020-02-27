a5d24871a0ca45e96a849e75d76f36e79eaa080d
Eesha <epatel@msudenver.edu>
2020-02-26 21:26:14 -0700

    hello world

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+root.title("rssticker")
+root.geometry("300x30")
+myLabel = Label(root, text="Accio, hello world!")
+myLabel.pack()
+myContainer = Frame(root)
+
+root.mainloop()
