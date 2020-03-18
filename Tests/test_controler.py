8f22b1906ae895d2d4ea2ca75b2adf12668db2d4
Lucas M <lucascube@gmail.com>
2020-03-18 15:41:48 -0600

    Simple mock for the controller.

---
+++ Tests/test_controler.py
@@ -0,0 +1,37 @@
+# https://ongspxm.github.io/blog/2016/11/assertraises-testing-for-errors-in-unittest/
+import unittest
+from unittest.mock import patch, Mock
+from Rss.controller.rssmanager import RssController
+
+
+class TestRssModel(unittest.TestCase):
+
+    def setUp(self):
+        self.rss=Mock()
+        self.view=Mock()
+        self.ctr=RssController()
+        self.rss_url_list=None
+        self.loaded_urls=['http://fake.com', 'http://anotherfake.com']
+        pass
+
+    def test_next_url(self):
+        ctr.urls = self.loaded_urls
+        _url = ctr.next_url()
+        assert _url == 'http://fake.com'
+        _url = ctr.next_url()
+        assert _url == 'http://anotherfake.com'
+        with self.assertRaises(Exception): ctr.next_url()
+
+    def test_next_url_fail(self):
+        ctr.urls = []
+        with self.assertRaises(Exception): ctr.next_url()
+
+    """This exception should be passed to the view, the view then should display the exception in a user friendly 
+    manner. """
+    def test_load_file_fail(self):
+        ctr.filename='NotRealFile.txt'
+        with self.assertRaises(Exception): ctr.load_file()
+
+
+
+
