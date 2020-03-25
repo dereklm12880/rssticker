0ff8e896f56271c9dcc567b94897c8b71dc49e08
Eesha <epatel@msudenver.edu>
2020-03-24 20:08:22 -0600

    test window

---
+++ tests/test_popup.py
@@ -0,0 +1,13 @@
+import unittest
+from RSS.view import userinterface
+
+class RSSWindow(unittest.TestCase):
+    def test_loop(self):
+        pass
+    def test_style(self):
+        pass
+    def test_build(self):
+        pass
+    def test_refresh(self):
+        pass
+        
