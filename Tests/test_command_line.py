b560303eb3a34ef21d05c890652c4856bb7f72c7
unknown <spacerobbie@gmail.com>
2020-04-07 13:27:24 -0600

    Tests/test_command_line.py

---
+++ Tests/test_command_line.py
@@ -0,0 +1,23 @@
+# https://docs.python.org/3.3/library/argparse.html
+
+import argparse
+import sys
+import unittest
+
+class TestArgs(unittest.TestCase):
+    def test_help(self):
+        sys.argv = ["thecastleargv", "--foo"]
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--foo', action='store_true')
+        args = parser.parse_args()
+
+        self.assertTrue('foo' in args)
+        self.assertFalse('bar' in args)
+        self.assertTrue(args.foo)
+
+    def test_empty_list(self):
+        sys.argv = ["thecastleargv"]
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--foo', action='append', default=[])
+        args = parser.parse_args()
+        self.assertEqual(args.foo, [])
