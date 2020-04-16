600113eda8ecf16f71397b1ce5019d0cc7680182
Eesha <epatel@msudenver.edu>
2020-04-15 20:47:06 -0600

    merging alanis branch

--- RSS/view/userinterface.py
+++ RSS/view/userinterface.py
@@ -5,23 +5,56 @@ import os
 import tkinter as tk
 from tkinter import ttk
 
-class RSSticker(tk.Frame):
 
-    def __init__(self,master=None):
+class RSSticker(tk.Frame):
+    headline = "[insert headline here]"
+
+    def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.popup_window = ttk.Label(master)
-    
+        self.popup_window = ttk.Label(self.master)
+        
         self.build_window()
+        self.build_menu()
         self.pack()
-    
+
     def start(self):
         self.master.mainloop()
 
     def build_window(self):
         self.popup_window.pack(side="top")
+        self.pack()
 
     def refresh(self, headline, link):
         print("headline:", headline, "\nlink:", link)
         self.popup_window.configure(text=headline)
         self.popup_window.configure("<Button-1>", lambda e: webbrowser.open_new(link))
+
+    def build_menu(self):
+        self.menu_bar = tk.Menu(self)
+        self.dropdown_menu = tk.Menu(self.menu_bar)
+        self.color_menu = tk.Menu(self.dropdown_menu)
+        list_colors = ["powder blue", "gray", "light green", "white"]
+        for color in list_colors:
+            self.color_menu.add_checkbutton(label=color, command=lambda arg0=color: RSSticker.background_color(arg0))
+        self.dropdown_menu.add_command(label="Cycle Time", command=RSSticker.cycle_time())
+        self.dropdown_menu.add_command(label="Window Placement", command=RSSticker.cycle_time())
+        self.dropdown_menu.add_cascade(label="Change Background Color", menu=self.color_menu)
+        self.menu_bar.add_cascade(label="Settings", menu=self.dropdown_menu)
+        self.master.config(menu=self.menu_bar)
+
+    def background_color(arg0):
+        self.master.config(background=arg0)
+
+    def cycle_time():
+        pass
+    
+    def window_placment():
+        pass
+
+#if __name__ == "__main__":
+    #root = tk.Tk()
+    #root.title("RSSticker")
+    #app = RSSticker(master=root)
+    #RSSticker.build_menu()
+   # app.mainloop()
--- Tests/test_userinterface.py
+++ Tests/test_userinterface.py
@@ -4,11 +4,12 @@ import webbrowser
 import tkinter as tk
 from tkinter import ttk
 import os, sys
+
 sys.path.append("../")
 from RSS.view import userinterface as ui
 
-class TestUI(unittest.TestCase):
 
+class TestUI(unittest.TestCase):
 
     def test_start(self):
         """Checks functionality of .mainloop()"""
@@ -17,8 +18,8 @@ class TestUI(unittest.TestCase):
             app = ui.RSSticker(master=root)
             app.start()
             mock_window.assert_has_calls(mock_window.mainloop())
-    
-    #https://github.com/drsjb80/MockingPython/blob/master/mocktk.py
+
+    # https://github.com/drsjb80/MockingPython/blob/master/mocktk.py
     def test_build_window(self):
         """Builds the window on the top left"""
         with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
@@ -29,7 +30,7 @@ class TestUI(unittest.TestCase):
             mock_window.assert_has_calls([
                 call().pack(side='top'),
             ], any_order=True)
-    
+
     def test_refresh(self):
         """Refreshes the cycled headlines and URLs, and opens in a new browser window"""
         with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
@@ -39,8 +40,11 @@ class TestUI(unittest.TestCase):
             link = 'www.google.com'
             app.refresh(headline, link)
             mock_window.assert_has_calls(mock_window.configure('Google'),
-                mock_window.bind("<Button-1", lambda e: webbrowser.open_new('www.google.com')))
+                                         mock_window.bind("<Button-1", lambda e: webbrowser.open_new('www.google.com')))
 
-    #def test_background(self):
-        #"""Customization of the popup window background"""
-        #with patch('RSS.view.userinterface.tk.Label', new_callable=PropertyMock) as mock_window:
+    def test_background_color(self):
+        with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
+            root = tk.Tk()
+            app = ui.RSSticker(master=root)
+            arg0 = "red"
+            mock_window.assert_has_calls(mock_window.config(background=arg0))
