9e709520743635e153213c32eaee6c7db94266cb
Alanis Kitchen <alaniskitchen47@gmail.com>
2020-04-18 18:10:01 -0500

    Updating branch

---
+++ RSS/controller/controler.py
@@ -0,0 +1,78 @@
+from RSS.model.rssfeed import RssModel
+# from RSS.view.rssticker import RssView # # This RssView is still unwritten as of now
+import csv
+import time
+import threading
+import concurrent.futures
+
+
+class RssController:
+    list_urls = []
+    list_iterator = None
+    rss_model = None
+    rss_view = None
+    url_index_pos = 0
+    filename = ''
+    cycle_time = 0
+
+    def __init__(self):
+        self.rss_model = RssModel()
+        # self.rss_view = RssView
+        self.filename = '../../list_urls.csv'
+
+    def load_urls(self):
+        try:
+            with open(self.filename, newline='') as f:
+                reader = csv.reader(f)  # this CSV is in the controller folder
+                self.list_urls = list(reader)
+                self.list_iterator = iter(self.list_urls)
+                return self.list_urls
+        except FileNotFoundError:
+            raise Exception("There is no file named " + self.filename + " in this directory")
+
+    def next_url(self):
+
+        if self.url_index_pos == 0:
+            self.url_index_pos = self.url_index_pos + 1
+            return self.list_urls[0]
+        try:
+            current_index = self.url_index_pos
+            self.url_index_pos = self.url_index_pos + 1
+            return self.list_urls[current_index]
+        except IndexError:
+            raise Exception("There are no more URL's!")
+
+    def next_feed(self, _url):
+
+        _rss_model = self.rss_model.parse(_url[0])
+
+        while self.rss_model._newsreel_index_pos < len(self.rss_model.newsreel):
+            if self.rss_model._newsreel_index_pos == 0:
+                _newsreel = _rss_model.get_current()
+                # pass newsreel into the view here
+                print(_newsreel)
+                # sleep x number of seconds?
+                time.sleep(self.cycle_time)
+                self.rss_model._newsreel_index_pos = self.rss_model._newsreel_index_pos + 1
+            else:
+                _newsreel = _rss_model.get_current()
+                self.rss_model._newsreel_index_pos = self.rss_model._newsreel_index_pos + 1
+                # pass newsreel to the view
+                print(_newsreel)
+                # sleep x number of seconds
+                time.sleep(self.cycle_time)
+
+        # return _newsreel
+
+    def main(self):
+        self.list_urls = self.load_urls()
+        if len(self.list_urls) == 0:
+            raise Exception("No URL's given")
+        for _ in self.list_urls:
+            self.rss_model._newsreel_index_pos = 0
+            _url = self.next_url()  # This gets the first url
+            self.next_feed(_url)
+
+
+if __name__ == "__main__":
+    RssController().main()
--- Tests/test_controler.py
+++ Tests/test_controler.py
@@ -1,37 +1,52 @@
 # https://ongspxm.github.io/blog/2016/11/assertraises-testing-for-errors-in-unittest/
 import unittest
 from unittest.mock import patch, Mock
-from Rss.controller.rssmanager import RssController
+from RSS.controller.controler import RssController
+from RSS.model.rssfeed import RssModel
 
 
 class TestRssModel(unittest.TestCase):
 
     def setUp(self):
-        self.rss=Mock()
-        self.view=Mock()
-        self.ctr=RssController()
-        self.rss_url_list=None
-        self.loaded_urls=['http://fake.com', 'http://anotherfake.com']
+        self.rss = RssModel()
+        self.view = Mock()
+        self.ctr = RssController()
+        self.rss_url_list = None
+        self.loaded_urls = ['http://fake.com', 'http://anotherfake.com']
+        self.load_feed_url = ['https://www.techrepublic.com/rssfeeds/articles/']
+        self.loaded_feed = self.rss.parse(self.load_feed_url[0])
+        self.test_feed = self.loaded_feed.get_current()
         pass
 
     def test_next_url(self):
-        ctr.urls = self.loaded_urls
-        _url = ctr.next_url()
+        self.ctr.list_urls = self.loaded_urls
+        _url = self.ctr.next_url()
         assert _url == 'http://fake.com'
-        _url = ctr.next_url()
+        _url = self.ctr.next_url()
         assert _url == 'http://anotherfake.com'
-        with self.assertRaises(Exception): ctr.next_url()
+        with self.assertRaises(Exception): self.ctr.next_url()
+
+    def test_feed(self):
+        _url = self.load_feed_url
+        _rss_model = self.ctr.rss_model.parse(_url[0])
+        _newsreel = _rss_model.get_current()
+        self.assertTrue(_newsreel, self.ctr.next_feed(_url))
 
     def test_next_url_fail(self):
-        ctr.urls = []
-        with self.assertRaises(Exception): ctr.next_url()
+        self.ctr.list_urls = []
+        with self.assertRaises(Exception): self.ctr.next_url()
 
     """This exception should be passed to the view, the view then should display the exception in a user friendly 
     manner. """
+
     def test_load_file_fail(self):
-        ctr.filename='NotRealFile.txt'
-        with self.assertRaises(Exception): ctr.load_file()
+        self.ctr.filename = 'NotRealFile.txt'
+        with self.assertRaises(Exception): self.ctr.load_urls()
+        self.ctr.filename = '../testing_urls.csv'
+        with self.assertRaises(Exception): self.ctr.main()
 
-
-
+    def test_load_urls(self):
+         self.ctr.filename = '../testing_urls.csv'
+         list_urls = self.ctr.load_urls()
+         self.assertIs(type(list_urls), list)
 
