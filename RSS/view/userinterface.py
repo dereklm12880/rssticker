9b682bd18307fc1b9fba0930ca370dd673db58e5
Eesha <epatel@msudenver.edu>
2020-04-22 15:14:33 -0600

    pulling from alanis

--- RSS/view/userinterface.py
+++ RSS/view/userinterface.py
@@ -4,9 +4,13 @@ import webbrowser
 import os
 import tkinter as tk
 from tkinter import ttk
+from RSS.controller.rssfeed import RssController
 
 
 class RSSticker(tk.Frame):
+    time = None
+    place = None
+    color = None
 
     def __init__(self, master=None):
         super().__init__(master)
@@ -28,40 +32,41 @@ class RSSticker(tk.Frame):
         self.popup_window.configure("<Button-1>", lambda e: webbrowser.open_new(link))
 
     def build_menu(self):
-        self.menu_bar = tk.Menu(self.popup_window)
-        color_menu = tk.Menu(self.menu_bar)
-        placement_menu = tk.Menu(self.menu_bar)
-        cycle_time_menu = tk.Menu(self.menu_bar)
-
+        menu_bar = tk.Menu(self.popup_window)
+        dropdown_menu = tk.Menu(menu_bar)
+        color_menu = tk.Menu(dropdown_menu)
+        placement_menu = tk.Menu(dropdown_menu)
+        cycle_time_menu = tk.Menu(dropdown_menu)
         list_colors = ["powder blue", "gray", "light green", "white"]
         list_placement = ["top left", "bottom left", "top right", "bottom right"]
-        cycle_options = [5, 10, 15, 20, 25, 30]
-
         for color in list_colors:
             color_menu.add_radiobutton(label=color, command=lambda arg0=color: RSSticker.background_color(self, arg0))
-        
+        cycle_options = [5, 10, 15, 20, 25, 30]
         for time in cycle_options:
             cycle_time_menu.add_radiobutton(label=time, command=lambda arg0=time: RSSticker.cycle_time(self, arg0))
-            
         for place in list_placement:
             placement_menu.add_radiobutton(label=place,
                                            command=lambda arg0=place: RSSticker.window_placement(self, arg0))
-
-        self.menu_bar.add_cascade(label="Cycle Time", menu=cycle_time_menu)
-        self.menu_bar.add_cascade(label="Window Placement", menu=placement_menu)
-        self.menu_bar.add_cascade(label="Change Background Color", menu=color_menu)
-        self.menu_bar.add_cascade(label="Settings", menu=self.menu_bar)
-        self.menu_bar.add_command(label="Save Settings", command=RSSticker.save())
-        # self.master.config(menu=menu_bar)
-
+        dropdown_menu.add_cascade(label="Cycle Time", menu=cycle_time_menu)
+        dropdown_menu.add_cascade(label="Window Placement", menu=placement_menu)
+        dropdown_menu.add_cascade(label="Change Background Color", menu=color_menu)
+        menu_bar.add_cascade(label="Settings", menu=dropdown_menu)
+        dropdown_menu.add_radiobutton(label="Save Settings",
+                                      command=lambda: RSSticker.save(self, {'color': RSSticker.color},
+                                                                     {'place': RSSticker.place},
+                                                                     {'time': RSSticker.time}))
+        self.master.config(menu=menu_bar)
 
     def background_color(self, arg0):
+        RSSticker.color = arg0
         self.master.configure(background=arg0)
 
     def cycle_time(self, arg0):
+        RSSticker.time = arg0
         pass
 
     def window_placement(self, arg0):
+        RSSticker.place = arg0
         if arg0 == "top left":
             self.master.geometry("+0+0")
         elif arg0 == "bottom left":
@@ -71,5 +76,17 @@ class RSSticker(tk.Frame):
         elif arg0 == "bottom right":
             self.master.geometry("+1000+750")
 
-    def save():
+    def save(self, color, place, time):
+        settings = [color, place, time]
+        RssController.save_settings(settings)
+        print(time)
+        print(place)
+        print(color)
         pass
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("RSSticker")
+    app = RSSticker(master=root)
+    app.mainloop()
--- Tests/test_userinterface.py
+++ Tests/test_userinterface.py
@@ -15,7 +15,7 @@ class TestUI(unittest.TestCase):
     def test_build_window(self):
         """Builds the window on the top left"""
         with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
-            root = tk.Tk()
+            root = mock_window.Tk()
             app = ui.RSSticker(master=root)
             app.build_window()
             mock_window.assert_has_calls([
@@ -25,61 +25,33 @@ class TestUI(unittest.TestCase):
     def test_refresh(self):
         """Refreshes the cycled headlines and URLs, and opens in a new browser window"""
         with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
-            root = tk.Tk()
+            root = mock_window.Tk()
             app = ui.RSSticker(master=root)
             headline = 'Google'
             link = 'www.google.com'
             app.refresh(headline, link)
             mock_window.assert_has_calls(mock_window.configure('Google'),
                                          mock_window.bind("<Button-1", lambda e: webbrowser.open_new('www.google.com')))
-    
-    def test_build_menu(self):
-        with patch('RSS.view.userinterface.tk.Menu', new_callable=PropertyMock) as mock_window:
-            root = tk.Tk()
-            app = ui.RSSticker(master=root)
-            app.build_menu()
-
-            list_colors = ['powder blue', 'gray', 'light green', 'white']
-            list_placement = ['top left', 'bottom left', 'top right', 'bottom right']
-            cycle_options = [5, 10, 15, 20, 25, 30]
-            
-            for color in list_colors:
-                mock_window.assert_has_calls(root.add_radiobutton(label=color, command=lambda arg0=color: RSSticker.background_color(self, arg0))
-            
-            for time in cycle_options:
-                mock_window.assert_has_calls(root.add_radiobutton(label=time, command=lambda arg0=time: RSSticker.cycle_time(self, arg0))
-            
-            for place in list_placement:
-                mock_window.assert_has_calls(root.add_radiobutton(label=place,
-                                           command=lambda arg0=place: RSSticker.window_placement(self, arg0))
-
-        self.menu_bar.add_cascade(label="Cycle Time", menu=cycle_time_menu)
-        self.menu_bar.add_cascade(label="Window Placement", menu=placement_menu)
-        self.menu_bar.add_cascade(label="Change Background Color", menu=color_menu)
-        self.menu_bar.add_cascade(label="Settings", menu=self.menu_bar)
-        self.menu_bar.add_command(label="Save Settings", command=RSSticker.save())
-        # self.master.config(menu=menu_bar)
-            #mock_window.assert_has_calls([
-              # dropdown menus have "powder blue", "gray", "light green", "white" options
-            #])
-
-    def test_background_color(self):
+    def test_cycle_time(self):
         with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
-            root = tk.Tk()
+            root = mock_window.Tk()
+            app = ui.RSSticker(master=root)
+            arg0 = 5
+            app.cycle_time(arg0)
+            self.assertIsNotNone(app.time)
+
+
+    def test_backgroundcolor(self):
+        with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
+            root = mock_window.Tk()
             app = ui.RSSticker(master=root)
             arg0 = "red"
             app.background_color(arg0)
             mock_window.assert_has_calls(mock_window.configure(background=arg0))
-    
-    def test_cycle_time(self):
-        with patch('RSS.view.userinterface.', new_callable=PropertyMock) as mock_window:
-            root = tk.Tk()
-            app = ui.RSSticker(master=root)
-            app.cyle_time()
 
     def test_window_placment(self):
         with patch('RSS.view.userinterface.ttk.Label', new_callable=PropertyMock) as mock_window:
-            root = tk.Tk()
+            root = mock_window.Tk()
             app = ui.RSSticker(master=root)
             list_placement = ["top left", "bottom left", "top right", "bottom right"]
             for place in list_placement:
@@ -92,9 +64,3 @@ class TestUI(unittest.TestCase):
                     mock_window.assert_has_calls(root.geometry("+1000+0"))
                 elif place == "bottom right":
                     mock_window.assert_has_calls(root.geometry("+1000+750"))
-
-    def test_save(self):
-        with patch('RSS.view.userinterface.', new_callable=PropertyMock) as mock_window:
-            root = tk.Tk()
-            app = ui.RSSticker(master=root)
-            app.save()
