a4ab7a371d59c924e0b31532ab86dd602bcdd870
Alanis Kitchen <alaniskitchen47@gmail.com>
2020-04-24 14:15:40 -0500

    removing syntax errors

--- RSS/model/rssfeed.py
+++ RSS/model/rssfeed.py
@@ -3,14 +3,11 @@ import feedparser
 
 
 class RssModel:
-<<<<<<< HEAD
     _newsreel_index_pos = 0
     _raw_feed = ''
 
-=======
     _newsreel_index_pos = -1
     _raw_feed = ''
->>>>>>> feature/alanis
     given_url = ''
     title = ''
     subtitle = ''
@@ -18,16 +15,13 @@ class RssModel:
     newsreel = []
 
     def parse(self, feed_url):
-<<<<<<< HEAD
         self._raw_feed = feedparser.parse(feed_url)
         if len(self._raw_feed) == 0:
             raise Exception("No feed with the url {} found.".format(feed_url))
-=======
         if not isinstance(feed_url, str): raise Exception('Expects string {} given'.format(type(feed_url)))
         self._raw_feed = feedparser.parse(feed_url)
         if len(self._raw_feed) == 0: raise Exception("No feed with the url {} found.".format(feed_url))
         if 'bozo' in self._raw_feed and self._raw_feed['bozo'] == 1: raise Exception("An unexpected issue occurred: {}".format(self._raw_feed['bozo_exception']))
->>>>>>> feature/alanis
         self.given_url = feed_url
         self.title = self._raw_feed['feed']['title']
         self.subtitle = self._raw_feed['feed']['subtitle']
@@ -37,7 +31,6 @@ class RssModel:
 
     def get_current(self):
         try:
-<<<<<<< HEAD
             return self.newsreel[self._newsreel_index_pos]
         except IndexError:
             raise Exception("There is no news loaded! Try parsing a new RSS feed.")
@@ -48,7 +41,6 @@ class RssModel:
             return self.newsreel[self._newsreel_index_pos]
         except IndexError:
             raise Exception("There is no more news! Try parsing a new RSS feed.")
-=======
             _tmp = None
             if self._newsreel_index_pos < 0:
                 _tmp = self._newsreel_index_pos
@@ -64,4 +56,3 @@ class RssModel:
             self._newsreel_index_pos = self._newsreel_index_pos +1
             return self.get_current()
         except IndexError: raise Exception("There is no more news! Try parsing a new RSS feed.")
->>>>>>> feature/alanis
