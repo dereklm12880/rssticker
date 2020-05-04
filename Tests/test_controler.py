aecad1ef88237caf9db7e65d900c6fa139e067e7
Eesha <epatel@msudenver.edu>
2020-05-03 20:21:48 -0600

    final documentation

--- Tests/test_controler.py
+++ Tests/test_controler.py
@@ -7,11 +7,9 @@ from RSS.model.rssfeed import RssModel
 from RSS.model.settings import SettingsModel
 
 
-#IS THIS REDUNDANT??? TEST_RSSFEED AND OR TEST_SETTINGS
-
 class TestRssModel(unittest.TestCase):
 
-    """Test class for RSS.model.settings.SettingsModel."""
+    """Test class for RSS.controller.rssfeed.RssController."""
 
     _return_value = {"feeds": ["http://fakefeed.com", "http://anotherfakefeed.com"]}
     _mock_open = mock.mock_open(read_data='')
@@ -21,7 +19,7 @@ class TestRssModel(unittest.TestCase):
 
     def setUp(self):
 
-        """Function that sets up the unittest for the RssModel"""
+        """Function that sets up the unittest for the RssController"""
 
         self.rss = RssModel()
         self.view = Mock()
@@ -34,16 +32,25 @@ class TestRssModel(unittest.TestCase):
         pass
 
     def test_next_feed(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.next_feed.
+        Test to check that the function returns the feeds parsed in the model.
+        """
         _next = self.ctr.next_feed()
         assert isinstance(_next, RssModel)
 
     def test_next_feed_fail_invalid_url(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.next_feed.
+        Test to check the function is throwing an exception because of an invalid url.
+        """
         with patch.object(SettingsModel, 'next_url', return_value="abc"):
             _ctr = RssController()
             with self.assertRaises(Exception):
                 _ctr.next_feed()
 
     def test_next_feed_fail_no_feeds(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.next_feed.
+        Test to check the function is throwing an exception because there are no feeds.
+        """
         self.ctr.settings_model.settings = {}
         with self.assertRaises(Exception): self.ctr.next_feed(self.load_feed_url)
 
@@ -51,6 +58,9 @@ class TestRssModel(unittest.TestCase):
         self.ctr.settings_model.settings = settings
 
     def test_save_settings_no_settings(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.save_settings.
+        Test to check the save settings function is saving settings into the yaml file with no settings.
+        """
         with patch.object(SettingsModel, 'load_settings', return_value={}) as mock_method:
             with patch.object(SettingsModel, 'save_settings', new=self._dump) as mock_method:
                 self.ctr.settings_model.settings = self.ctr.settings_model.load_settings()
@@ -59,6 +69,9 @@ class TestRssModel(unittest.TestCase):
                 assert self.ctr.settings_model.settings == settings
 
     def test_save_settings_with_settings_no_feeds(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.save_settings.
+        Test to check the save settings function is saving settings into the yaml file with no feeds.
+        """
         with patch.object(SettingsModel, 'load_settings',
                           return_value=self._dummy_yaml_file_settings_no_feeds) as mock_method:
             with patch.object(SettingsModel, 'save_settings', new=self._dump) as mock_method:
@@ -69,6 +82,9 @@ class TestRssModel(unittest.TestCase):
                 assert self.ctr.settings_model.settings == settings
 
     def test_save_settings_with_settings_with_feeds(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.save_settings.
+        Test to check the save settings function is saving settings into the yaml file with feeds.
+        """
         with patch.object(SettingsModel, 'load_settings',
                           return_value=self._dummy_yaml_file_settings_with_feeds) as mock_method:
             with patch.object(SettingsModel, 'save_settings', new=self._dump) as mock_method:
@@ -79,6 +95,9 @@ class TestRssModel(unittest.TestCase):
                 assert self.ctr.settings_model.settings == {'feeds': ['https://fake.com']}
 
     def test_save_settings_with_settings_with_feeds_with_other(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.save_settings.
+        Another test to check the save settings function is saving settings into the yaml file with feeds.
+        """
         self.ctr.settings_model.filename = 'dummy.yaml'
         with patch.object(SettingsModel, 'load_settings',
                           return_value=self._dummy_yaml_file_settings_with_feeds_and_others) as mock_method:
@@ -91,6 +110,9 @@ class TestRssModel(unittest.TestCase):
                                                             'feeds': ['https://fake.com']}
 
     def test_save_settings_with_settings_feeds(self):
+        """ Unit test for RSS.controller.rssfeed.RssController.save_settings.
+        Another test to check the save settings function is saving settings into the yaml file with feeds.
+        """
         self.ctr.settings_model.filename = 'dummy.yaml'
         with patch.object(SettingsModel, 'load_settings',
                           return_value=self._dummy_yaml_file_settings_with_feeds_and_others) as mock_method:
--- Tests/test_settings.py
+++ Tests/test_settings.py
@@ -15,9 +15,11 @@ class TestRssSettings(unittest.TestCase):
     _mock_open = mock.mock_open(read_data='')
 
     def test_load_settings_no_file(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.load_settings.
+
+        """ Unit test for RSS.model.settings.SettingsModel.load_settings.
         Tests to make sure that the .yaml file exists.
         """
+
         with patch.object(yaml, 'load', return_value=self._return_value) as mock_method:
             with self.assertRaises(Exception):
                 _settings = SettingsModel()
@@ -25,9 +27,11 @@ class TestRssSettings(unittest.TestCase):
                 _settings.load_settings()
 
     def test_load_settings(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.next_url.
+
+        """ Unit test for RSS.model.settings.SettingsModel.next_url.
         Tests to check feeds in yaml file that can be loaded.
         """
+
         with patch.object(yaml, 'load', return_value=self._return_value) as mock_method:
             with mock.patch('builtins.open', self._mock_open):
                 _settings = SettingsModel()
@@ -36,9 +40,11 @@ class TestRssSettings(unittest.TestCase):
                 assert _loaded_settings['feeds'] == self._return_value['feeds']
 
     def test_save_settings(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+
+        """ Unit test for RSS.model.settings.SettingsModel.save_settings.
         Tests to check a dictionary of configurable values to be converted into the yaml file. 
         """
+
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         _settings.save_settings(self._return_value)
@@ -46,9 +52,11 @@ class TestRssSettings(unittest.TestCase):
         os.remove(_settings.filename)
 
     def test_save_settings_again(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+
+        """ Unit test for RSS.model.settings.SettingsModel.save_settings.
         Tests to check a dictionary of configurable values to be converted into the yaml file. 
         """
+
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         _settings.settings = self._return_value
@@ -57,9 +65,11 @@ class TestRssSettings(unittest.TestCase):
         os.remove(_settings.filename)
 
     def test_save_settings_fail_set_via_settings_class_member(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+
+        """ Unit test for RSS.model.settings.SettingsModel.save_settings.
         Tests to check the function is throwing an exception because of the settings class.
         """
+
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         _settings.settings = 'Bob dole was here'
@@ -67,18 +77,22 @@ class TestRssSettings(unittest.TestCase):
             _settings.save_settings()
 
     def test_save_settings_fail_set_via_argument(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+
+        """ Unit test for RSS.model.settings.SettingsModel.save_settings.
         Tests to check the function is throwing an exception because the argument is invalid.
         """
+
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         with self.assertRaises(Exception):
             _settings.save_settings(12345)
 
     def test_next_url(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.next_url.
+        
+        """ Unit test for RSS.model.settings.SettingsModel.next_url.
         Tests to assert that are are valid feeds in the yaml file.
         """
+
         with patch.object(yaml, 'load', return_value=self._return_value) as mock_method:
             _settings = SettingsModel()
             _settings.filename = 'dummy.yaml'
@@ -91,9 +105,11 @@ class TestRssSettings(unittest.TestCase):
             assert _url == 'http://fakefeed.com'
 
     def test_next_url_with_exception(self):
-        """ Unit test for RSSmodel.model.settings.SettingsModel.next_url.
+
+        """ Unit test for RSS.model.settings.SettingsModel.next_url.
         Tests to check that the function is throwing an exception when the return value is empty.
         """
+        
         with patch.object(yaml, 'load', return_value={}) as mock_method:
             _settings = SettingsModel()
             _settings.filename = 'dummy.yaml'
