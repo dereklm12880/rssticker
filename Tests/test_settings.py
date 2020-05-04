71d3f9ab095aa20e92f8cac6df15eb11197885b5
Eesha <epatel@msudenver.edu>
2020-05-03 19:40:14 -0600

    final documentation

--- Tests/test_settings.py
+++ Tests/test_settings.py
@@ -36,6 +36,9 @@ class TestRssSettings(unittest.TestCase):
                 assert _loaded_settings['feeds'] == self._return_value['feeds']
 
     def test_save_settings(self):
+        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+        Tests to check a dictionary of configurable values to be converted into the yaml file. 
+        """
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         _settings.save_settings(self._return_value)
@@ -43,6 +46,9 @@ class TestRssSettings(unittest.TestCase):
         os.remove(_settings.filename)
 
     def test_save_settings_again(self):
+        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+        Tests to check a dictionary of configurable values to be converted into the yaml file. 
+        """
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         _settings.settings = self._return_value
@@ -51,6 +57,9 @@ class TestRssSettings(unittest.TestCase):
         os.remove(_settings.filename)
 
     def test_save_settings_fail_set_via_settings_class_member(self):
+        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+        Tests to check the function is throwing an exception because of the settings class.
+        """
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         _settings.settings = 'Bob dole was here'
@@ -58,12 +67,18 @@ class TestRssSettings(unittest.TestCase):
             _settings.save_settings()
 
     def test_save_settings_fail_set_via_argument(self):
+        """ Unit test for RSSmodel.model.settings.SettingsModel.save_settings.
+        Tests to check the function is throwing an exception because the argument is invalid.
+        """
         _settings = SettingsModel()
         _settings.filename = 'dummy_.yaml'
         with self.assertRaises(Exception):
             _settings.save_settings(12345)
 
     def test_next_url(self):
+        """ Unit test for RSSmodel.model.settings.SettingsModel.next_url.
+        Tests to assert that are are valid feeds in the yaml file.
+        """
         with patch.object(yaml, 'load', return_value=self._return_value) as mock_method:
             _settings = SettingsModel()
             _settings.filename = 'dummy.yaml'
@@ -76,6 +91,9 @@ class TestRssSettings(unittest.TestCase):
             assert _url == 'http://fakefeed.com'
 
     def test_next_url_with_exception(self):
+        """ Unit test for RSSmodel.model.settings.SettingsModel.next_url.
+        Tests to check that the function is throwing an exception when the return value is empty.
+        """
         with patch.object(yaml, 'load', return_value={}) as mock_method:
             _settings = SettingsModel()
             _settings.filename = 'dummy.yaml'
